978620090a17e2d4345b2cd7bd5672f704445b9c
nehagaikwad1611 <154221811+nehagaikwad1611@users.noreply.github.com>
2025-09-23 22:47:01 +0530

    Delete Test2 directory

--- Test2/question2.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter three digit number: "))
-d1 = num % 10
-d2 =(num // 10) % 10
-d3 = num // 100
-
-if(d2 == 2*d1 and d3 == d1//2):
-    print(f"Yes, you have done it.")
-else:
-    ("Please try next time")
--- Test2/question5.py
+++
@@ -1,6 +0,0 @@
-p1 = int(input("Enter the price of first product: "))
-p2 = int(input("Enter the price of second product: "))
-p3 = int(input("Enter the price of third product:  "))
-p4 = int(input("Enter the price of fourth product: "))
-p5 = int(input("Enter the price of fifth product: "))
-
